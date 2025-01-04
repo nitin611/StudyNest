4bff33f346af8fc27f70a734416848b9a14099ee
nitin611 <jhanitin906@gmail.com>
2025-01-04 21:17:30 +0530

    adding rating and review

--- controllers/RatingAndReview.js
+++ controllers/RatingAndReview.js
@@ -1,8 +1,21 @@
 const RatingAndRevies=require("../Models/RatingAndRevie")
 const course=require("../Models/Course")
 
-// createRating-
-
+// ----------------------------------createRating--------------------------------------------
+exports.createRating=async(req,res)=>{
+    try {
+        
+    }
+     catch (err) {
+        console.log(err)
+        return res.status(500).send({
+            success:false,
+            msg:'Error in creating Rating'
+        })
+        
+        
+    }
+}
     // fetch details-
 
     // 
