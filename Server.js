fa5f94d9fb8ce8a220bc168f93707ce56d537d13
nitin611 <jhanitin906@gmail.com>
2025-01-09 15:23:08 +0530

    adding server.js file

--- Server.js
+++ Server.js
@@ -1,16 +1,37 @@
 const express=require('express')
 const app=express();
-require("dotenv").config
 const userRoutes=require("./Routes/User")
 const profileRoutes=require("./Routes/Profile")
 const paymentRoutes=require("./Routes/payments")
 const courseRoutes=require("./routes/Course")
-
 const database=require("./config/database")
-const cokkieParser=require("cookie-parser")
+const cookieParser=require("cookie-parser")
 const cors=require("cors")
 const {cloudinaryConnect}=require("./config/cloudinary");
+const fileUpload=require("express-fileupload")
+const dotenv=require("dotenv")
 
+require("dotenv").config
+const PORT=process.env.PORT || 4000;
 
+// database connection-
+database.connect()
 
-app.listen()
+// middleWares-
+app.use(express.json())
+app.use(cookieParser())
+// ------------------cors for frontend connection-------------------
+app.use(
+    cors({
+        origin:"http://localhost:3000",
+        credentials:true
+    })
+)
+app.use({
+    useTempFiles:true,
+    path:
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+});
