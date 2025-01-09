348ed6230fb91ad5ae0076e56b40c6580b399628
nitin611 <jhanitin906@gmail.com>
2025-01-09 15:35:06 +0530

    adding User Routes

---
+++ Routes/User.js
@@ -0,0 +1,13 @@
+const { Router } = require("express")
+const {signIn,signUp,sendOTP,changePassword}=require("../controllers/Auth")
+const {resetPassword,resetPasswordToken}=require("../controllers/ResetPassword")
+
+const {auth}=require("../middleware/auth")
+
+// ROUTES for login signup and authentication-
+
+// Route for login-
+Router.post("/login",signIn)
+Router.post("/signUp",signUp)
+Router.post("/changePassowrd",changePassword)
+Router.post("/sendOtp",sendOTP)
