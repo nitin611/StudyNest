f5929362b57687c0d6c6dafcd9190306c2345bf8
nitin611 <jhanitin906@gmail.com>
2025-01-12 16:05:10 +0530

    adding course page

--- Routes/course.js
+++ Routes/course.js
@@ -5,7 +5,7 @@ const {createCategory,getAllCategorys,CategoryPageDetails}=require("../controlle
 const {createRating,getAvgRating,getAllRating}=require("../controllers/RatingAndReview")
 const {createSection,updateSection,deleteSection}=require("../controllers/Section")
 const {createSubSection,updatedSubSection,deleteSubSection}=require("../controllers/subSection")
-const {auth,isAdmin,isInstructor}=require("../middleware/auth")
+const {auth,isAdmin,isInstructor,isStudent}=require("../middleware/auth")
 // Routes for course-
 Router.post("/createCourse",auth,isInstructor,createCourse)
 Router.get("/getAllCourse",getAllCourse)
@@ -17,19 +17,19 @@ Router.get("/getAllCategory",getAllCategorys)
 Router.post("/categoryDetails",CategoryPageDetails)
 
 // Routes for Rating and Reviews-
-Router.post("/createRating",createRating)
+Router.post("/createRating",isStudent,createRating)
 Router.get("/getRating",getAvgRating)
 Router.get("/allRating",getAllRating)
 
 // Routes for sections-
-Router.post("/createSection",auth,createSection)
-Router.put("/updateSection",auth,updateSection)
-Router.delete("/deleteSection/:id",auth,deleteSection)
+Router.post("/createSection",auth,isInstructor,createSection)
+Router.put("/updateSection",auth,isInstructor,updateSection)
+Router.delete("/deleteSection/:id",auth,isInstructor,deleteSection)
 
 // Routes for sub-Section-
-Router.post("/createSubSection",auth,createSubSection)
-Router.put("/updateSubSection",updatedSubSection)
-Router.delete("/deleteSubSection",deleteSubSection)
+Router.post("/createSubSection",auth,isInstructor,createSubSection)
+Router.put("/updateSubSection",auth,isInstructor,updatedSubSection)
+Router.delete("/deleteSubSection",auth,isInstructor,deleteSubSection)
 
 
 module.exports=Router
